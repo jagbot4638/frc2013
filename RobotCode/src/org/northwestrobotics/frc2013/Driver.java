bd3fe9bcc547a60b9876af0a6fd06445d7cbf444
Saagar Ahluwalia <soggy.potato@outlook.com>
2013-01-22 18:39:20 -0500

    Switched to java
    - Added necessary class structure
    - Empty framework to place remaining code in

---
+++ RobotCode/src/org/northwestrobotics/frc2013/Driver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.northwestrobotics.frc2013;
+
+/**
+ *
+ * @author Saagar
+ */
+class Driver {
+    public Driver(){
+        
+    }
+    
+    public void drive(){
+        
+    }
+}
