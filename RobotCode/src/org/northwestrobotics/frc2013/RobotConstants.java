e7754ef390fac85e9fc5ee5b43780b7e87d96809
Saagar Ahluwalia <soggy.potato@outlook.com>
2013-01-22 19:41:36 -0500

    Added RobotConstants class
    - This class is a commonplace to put all constant values
    - Added constant values for driving
    - Subclasses define subsystems, which have constants, within the robot

---
+++ RobotCode/src/org/northwestrobotics/frc2013/RobotConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.northwestrobotics.frc2013;
+
+/**
+ *
+ * @author Saagar
+ */
+public class RobotConstants {
+    public static final class Drive {
+        private Drive(){
+        }
+        public static final double threshold = 0.5;
+        
+        public static final int leftMotor = 1;
+        public static final int rightMotor = 2;
+    }
+}
