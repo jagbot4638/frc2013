123a78743fa10b2432c8e390f96d98614364ff3b
Saagar Ahluwalia <soggy.potato@outlook.com>
2013-01-22 19:18:14 -0500

    Basic API structure for Driver class
    - All the methods necessary
    - Organized based on design
    - Pseudocode instructions
    - Constant threshold to be below in change in velocity

--- RobotCode/src/org/northwestrobotics/frc2013/Driver.java
+++ RobotCode/src/org/northwestrobotics/frc2013/Driver.java
@@ -9,11 +9,35 @@ package org.northwestrobotics.frc2013;
  * @author Saagar
  */
 class Driver {
+    
+    final double threshold = .5;
+    
+    double joystickX = 0.0;
+    double joystickY = 0.0;
+    
     public Driver(){
         
     }
     
     public void drive(){
-        
+        double previousJoystickX = joystickX;
+        double previousJoystickY = joystickY;
+        userInput();
+        minimizeMotorDamage(previousJoystickX, previousJoystickX);
+        moveMotors();
+    }
+
+    private void userInput() {
+        // joystickX = ...;
+        // joystickY = ...;
+    }
+
+    private void minimizeMotorDamage(double previousJoystickX, double
+            previousJoystickX1) {
+        // if (joystickX - previousJoystickX ...) joystickX = ...;
+    }
+
+    private void moveMotors() {
+        // arcadeDrive(joystickX, joystickY);
     }
 }
