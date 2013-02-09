9c3b9725268eddef91c058877ad0d1c2c1a08bcf
Saagar Ahluwalia <soggy.potato@outlook.com>
2013-02-09 12:10:26 -0500

    awaiting user input change

--- RobotCode/src/org/northwestrobotics/frc2013/shooter/AwaitingUserInputState.java
+++ RobotCode/src/org/northwestrobotics/frc2013/shooter/AwaitingUserInputState.java
@@ -6,15 +6,25 @@ package org.northwestrobotics.frc2013.shooter;
 
 import org.northwestrobotics.frc2013.State;
 
+/**
+ * This is the initial state for the Shooter. This represents the shooter
+ * waiting for the user to command the robot to shoot.
+ *
+ * @author soggy.potato
+ */
+public class AwaitingUserInputState extends ShooterState {
 
-public class AwaitingUserInputState implements State {
-    public AwaitingUserInputState(Shooter shooter){
+    public AwaitingUserInputState(Shooter shooter) {
         super(shooter);
     }
+
     public void enter() {
     }
 
     public State handle() {
+        if (shooter.shootButtonPressed()) {
+            return shooter.getShootingState();
+        }
+        return this;
     }
-    
 }
