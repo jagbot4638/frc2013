48a8df87346f9bc77c4d9a8c48077712089ce36a
Saagar Ahluwalia <soggy.potato@outlook.com>
2013-02-16 14:58:41 -0500

    Added classes necessary for status displayer
    - Added DecimalData class and the interface it implements, DisplayData

---
+++ RobotCode/src/org/northwestrobotics/frc2013/DisplayData.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.northwestrobotics.frc2013;
+
+/**
+ *
+ * @author AgentOrange
+ */
+public interface DisplayData {
+    public String getFormattedString(int zoneLength);
+}
